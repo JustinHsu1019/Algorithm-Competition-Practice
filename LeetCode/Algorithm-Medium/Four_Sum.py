3a812c16a9aa9eb15b2ff0f98b3c00858bc7978a
Shoejust <141555665+JustinHsu1019@users.noreply.github.com>
2023-11-04 00:32:14 +0800

    Update LeetCode

--- LeetCode/Algorithm-Medium/Four_Sum.py
+++ LeetCode/Algorithm-Medium/Four_Sum.py
@@ -1,43 +1,46 @@
 # https://leetcode.com/problems/4sum
 
-# 進行中
-
 from typing import List
 
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
         nums.sort()
         result = []
-        for j in range(len(nums)):
-            if j > 0 and nums[j] == nums[j-1]:
+        length = len(nums)
+
+        for i in range(length - 3):
+            if i > 0 and nums[i] == nums[i - 1]:
                 continue
-            for i in range(len(nums)):
-                if i == j:
+            
+            for j in range(i + 1, length - 2):
+                if j > i + 1 and nums[j] == nums[j - 1]:
                     continue
-                if i > 0 and nums[i] == nums[i-1]:
-                    continue
-                left, right = i + 1, len(nums) - 1
+
+                left, right = j + 1, length - 1
+
                 while left < right:
-                    sum_ = nums[j] + nums[i] + nums[left] + nums[right]
-                    if sum_ == target:
-                        if j != left and j != right:
-                            temp = sorted([nums[j], nums[i], nums[left], nums[right]])
-                            if temp not in result:
-                                result.append(temp)
+                    current_sum = nums[i] + nums[j] + nums[left] + nums[right]
+
+                    if current_sum == target:
+                        quadruplet = [nums[i], nums[j], nums[left], nums[right]]
+                        result.append(quadruplet)
+
                         while left < right and nums[left] == nums[left + 1]:
                             left += 1
                         while left < right and nums[right] == nums[right - 1]:
                             right -= 1
+
                         left += 1
                         right -= 1
-                    elif sum_ < target:
+                    elif current_sum < target:
                         left += 1
                     else:
                         right -= 1
+
         return result
 
 def main():
-    quest = [2,2,2,2,2]
+    quest = [2, 2, 2, 2, 2]
     target = 8
     sol = Solution()
     ans = sol.fourSum(quest, target)
