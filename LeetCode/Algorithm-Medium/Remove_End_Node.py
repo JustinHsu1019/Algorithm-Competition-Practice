9ac685c63c1f46ff7d743e705fe1e679da4ce06f
Shoejust <141555665+JustinHsu1019@users.noreply.github.com>
2023-11-04 23:36:42 +0800

    Update LeetCode

--- LeetCode/Algorithm-Medium/Remove_End_Node.py
+++ LeetCode/Algorithm-Medium/Remove_End_Node.py
@@ -1,7 +1,5 @@
 # https://leetcode.com/problems/remove-nth-node-from-end-of-list
 
-# 進行中
-
 from typing import Optional
 
 # Definition for singly-linked list.
@@ -12,14 +10,39 @@ class ListNode:
 
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-        pass
+        current = head
+        head_list = []
+        while current:
+            head_list.append(current.val)
+            current = current.next
+        head_list.pop(-1*n)
+
+        if not head_list:
+            return None
+
+        dummy = ListNode(head_list[0])
+        current = dummy
+
+        for i in range(1, len(head_list)):
+            current.next = ListNode(head_list[i])
+            current = current.next
+
+        return dummy
 
 def main():
-    # Create a linked list: l1 = 2 -> 4 -> 9
-    l1 = ListNode(2, ListNode(4, ListNode(9)))
+    # Create a linked list: head = 2 -> 4 -> 9
+    head = ListNode(2, ListNode(4, ListNode(9)))
+    n = 1
 
     sol = Solution()
-    ans = sol.removeNthFromEnd(l1)
+    ans = sol.removeNthFromEnd(head, n)
+
+    current = ans
+    result = []
+    while current:
+        result.append(current.val)
+        current = current.next
+    print(result)
 
 if __name__ == "__main__":
     main()
